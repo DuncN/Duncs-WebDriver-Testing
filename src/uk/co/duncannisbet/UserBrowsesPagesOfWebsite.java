2a4624c5258a7fdf0cb8fcbac3d2d4a995e9300e
Duncan <duncan.nisbet@gmail.com>
2011-12-21 16:09:32 +0000

    added another Xpath example for clicking on carousel

--- src/uk/co/duncannisbet/UserBrowsesPagesOfWebsite.java
+++ src/uk/co/duncannisbet/UserBrowsesPagesOfWebsite.java
@@ -1,4 +1,6 @@
 //TODO - refactor class to follow Page Object Pattern
+// I've only included comments to help me learn
+
 package uk.co.duncannisbet;
 
 import static org.hamcrest.core.Is.is;
@@ -17,13 +19,16 @@ public class UserBrowsesPagesOfWebsite {
     private static WebDriver driver;
     private WebElement navBarMenu;
     private WebElement banner;
+    private WebElement carousel;
 
     @BeforeClass
+    //open browser
     public static void beforeClass() {
         driver = new FirefoxDriver();
     }
 
     @Test
+    //smoke test to prove site is actually up
     public void homepageTitleIsCorrect() {
 
         givenUserIsOnPage("http://www.duncannisbet.co.uk/");
@@ -32,6 +37,7 @@ public class UserBrowsesPagesOfWebsite {
     }
 
     @Test
+    //finding an element & clicking on it #1
     public void userNavigatesFromHomePageToWhoIAmPage() {
         givenUserIsOnPage("http://www.duncannisbet.co.uk/");
         whenUserClicksOnNavLink("menu-item-93");
@@ -40,6 +46,7 @@ public class UserBrowsesPagesOfWebsite {
     }
 
     @Test
+    //finding an element & clicking on it #2
     public void userNavigatesFromWhoIAmPageToHowITest() {
         givenUserIsOnPage("http://www.duncannisbet.co.uk/who-is-duncan-nisbet");
         whenUserClicksOnNavLink("menu-item-91");
@@ -47,6 +54,16 @@ public class UserBrowsesPagesOfWebsite {
     }
 
     @Test
+    //trying out Xpath #1
+    public void userNavigatesFromHomePageToClientPageUsingCarousel() {
+        givenUserIsOnPage("http://www.duncannisbet.co.uk/");
+        whenUserClicksCarouselLink();
+        thenPageIsDisplayed("Autotrader | Duncan Nisbet");
+
+    }
+
+    @Test
+    //trying out Xpath #2
     public void userClickingOnBannerIsNavigatedToHome() {
         givenUserIsOnPage("http://www.duncannisbet.co.uk/what-i-test");
         whenUserClicksOnBanner();
@@ -68,11 +85,17 @@ public class UserBrowsesPagesOfWebsite {
         banner.click();
     }
 
+    private void whenUserClicksCarouselLink() {
+        carousel = driver.findElement(By.xpath("//a[@title='Autotrader']/img"));
+        carousel.click();
+    }
+
     private void thenPageIsDisplayed(String page) {
         assertThat("Page Title is wrong", driver.getTitle(), is(page));
     }
 
     @AfterClass
+    //close browser
     public static void afterClass() {
         driver.close();
     }
